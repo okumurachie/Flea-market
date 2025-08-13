9ba10b9e3b3305cbbe7e768fa14cf1691a60c706
okumura chie <neco9145@me.com>
2025-08-13 21:06:13 +0900

    メール認証後リダイレクトテスト作成

--- src/tests/Feature/RegisterTest.php
+++ src/tests/Feature/RegisterTest.php
@@ -4,9 +4,13 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Facades\Notification;
+use Symfony\Component\DomCrawler\Crawler;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 use App\Models\User;
 
@@ -117,5 +121,59 @@ class RegisterTest extends TestCase
 
         $response = $this->get('/register');
         $response->assertSee('パスワードと一致しません');
+    }
+
+    public function test_register_sends_verification_email()
+    {
+        Notification::fake();
+
+        $response = $this->from('/register')->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'abcd1234',
+            'password_confirmation' => 'abcd1234',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $user = User::where('email', 'test@example.com')->first();
+        $this->assertTrue(Hash::check('abcd1234', $user->password));
+
+        Notification::assertSentTo($user, VerifyEmail::class);
+
+        $response->assertRedirect('email/verify');
+    }
+
+    public function test_register_email_verification_shows_navigation_and_redirects_authentication_page()
+    {
+        $user = User::factory()->create(['email_verified_at' => null]);
+        $response = $this->actingAs($user)->get('/email/verify');
+        $response->assertStatus(200);
+        $response->assertSee('認証はこちらから');
+
+        $crawler = new Crawler($response->getContent());
+
+        $link = $crawler->filter('a.verify-email__link')->attr('href');
+
+        $this->assertSame('http://localhost:8025', $link);
+    }
+
+    public function test_user_without_profile_redirects_to_profile_setting_page()
+    {
+        $user = User::factory()->create([
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+        ]);
+
+        $response = $this->followingRedirects()->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertSee('プロフィール設定');
     }
 }
