ae4fa9c8ae3d3b7df622774349774d158fa09d1b
okumura chie <neco9145@me.com>
2025-08-26 17:30:03 +0900

    出品情報登録テスト修正

--- src/tests/Feature/RegisterItemTest.php
+++ src/tests/Feature/RegisterItemTest.php
@@ -3,10 +3,8 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
 use App\Models\User;
 use App\Models\Item;
 use App\Models\Category;
@@ -26,7 +24,7 @@ class RegisterItemTest extends TestCase
         $user = User::factory()->create();
         $this->actingAs($user);
 
-        $condition = Condition::create(['id' => 1, 'condition' => '良好']);
+        Condition::create(['id' => 1, 'condition' => '良好']);
         $category1 = Category::create(['id' => 1, 'content' => 'ファッション']);
         $category2 = Category::create(['id' => 2, 'content' => '家電']);
 
